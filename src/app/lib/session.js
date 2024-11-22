e3c3025bf7fbf37d787551887afd23fc991deeac
Gabriel <gabrielcolomoll@gmail.com>
2024-11-22 18:22:10 +0100

    feature/getUserRoles

--- src/app/lib/session.js
+++ src/app/lib/session.js
@@ -1,14 +1,22 @@
 import "server-only";
 import { SignJWT, jwtVerify } from "jose";
 import { cookies } from "next/headers";
+import { findFiltered } from "../users/actions";
+import { getUserRoleById } from "../userRoles/actions";
 
 const secretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(secretKey);
 
+
 async function createSession(userId) {
+  const user = await findFiltered(userId);
+  const userRole = await getUserRoleById(user?.userRoleId);
+  console.log({ user, userRole });
+  const permissions = userRole.permissions;
+
   // 7 Days
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-  const session = await encrypt({ userId, expiresAt });
+  const session = await encrypt({ userId, expiresAt, permissions: permissions });
 
   //In future, it doesn't have to do the await
   const cookieStore = await cookies();
@@ -17,21 +25,21 @@ async function createSession(userId) {
     secure: true,
     sameSite: "Strict",
     expires: expiresAt,
+    maxAge: 7 * 24 * 60 * 60,
   });
+  cookieStore.set("userId", userId, {
+    secure: true,     // Solo en HTTPS
+    sameSite: "Strict",
+    expires: expiresAt,
+  });
+
   return cookieStore;
 }
 
-async function getUserIdFromSession() {
-  const session = cookies().get("session")?.value;
-
-  if (!session) return null;
-
-  const payload = await decrypt(session);
-  return payload?.userId || null;
-}
-
 async function deleteSession() {
-  cookies().delete("session");
+  const cookiesInstance = await cookies();
+  cookiesInstance.delete("userId");
+  cookiesInstance.delete("session");
 }
 
 async function encrypt(payload) {
@@ -49,8 +57,19 @@ async function decrypt(session = "") {
     });
     return payload;
   } catch {
-    console.log("Failed to verify session");
+    throw new Error("Invalid or expired session.");
   }
 }
 
-export { createSession, deleteSession, getUserIdFromSession, encrypt, decrypt };
+async function checkPermission(session, requiredPermission, userId) {
+  // Decodificar el JWT
+  const payload = await decrypt(session);
+
+  if (!payload || payload.userId !== userId || !payload.permissions || !payload.permissions.includes(requiredPermission)) {
+    throw new Error("No tienes los permisos necesarios para realizar esta acción.");
+  }
+
+  return true;
+}
+
+export { createSession, deleteSession, encrypt, decrypt, checkPermission };
