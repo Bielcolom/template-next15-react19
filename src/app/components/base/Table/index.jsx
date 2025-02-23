9a307cc214e048f7ac000ea4ce295820e8bcfeca
Gabriel <gabrielcolomoll@gmail.com>
2025-02-23 12:59:18 +0100

    refactor/table

--- src/app/components/base/Table/index.jsx
+++ src/app/components/base/Table/index.jsx
@@ -1,108 +1,89 @@
-// Table.js
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
-import Pagination from "./Pagination"; // Import the Pagination component
+import { useEffect, useState } from "react";
+import Icon from "../Icon";
 import styles from "./table.module.scss";
 
-const Table = ({
-    elements,
-    showActions,
-    extraActions,
-    loading = false,
-    itemsPerPage = 10,
-    className = "",
-    children,
-}) => {
-    const [currentPage, setCurrentPage] = useState(0);
-    const [paginatedItems, setPaginatedItems] = useState([]);
+const Table = ({ data, visibleColumns = [], loading }) => {
+    const [visibleRows, setVisibleRows] = useState(500);
 
     useEffect(() => {
-        // Paginate the elements when the page or elements change
-        const startIndex = currentPage * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        setPaginatedItems(elements.slice(startIndex, endIndex));
-    }, [currentPage, elements, itemsPerPage]);
+        document.querySelector(`.${styles.tableContainer}`).scrollTop = 0;
+        setVisibleRows(500);
+    }, [data]);
 
-    // Calculate total pages
-    const totalPages = Math.ceil(elements.length / itemsPerPage);
+    const handleScroll = (e) => {
+        const { scrollTop, clientHeight, scrollHeight } = e.target;
+        if (scrollTop + clientHeight >= scrollHeight) {
+            setVisibleRows((prev) => prev + 500);
+        }
+    };
 
-    // Get columns dynamically from the keys of the first object
-    const columns = elements[0] ? Object.keys(elements[0]) : [];
+    // Obtener las columnas automáticamente a partir de los datos
+    const columns = data.length > 0 ? Object.keys(data[0]).map((key) => ({ value: key, label: key })) : [];
+
+    const isColumnVisible = (column) => {
+        // Si no se pasa visibleColumns, todas las columnas son visibles por defecto
+        return visibleColumns.length === 0 || visibleColumns.includes(column);
+    };
 
     return (
-        <div className={`${styles["table-container"]} ${className}`}>
-            {loading ? (
-                <div className={styles.loading}>Loading...</div>
-            ) : (
-                <>
-                    <table className={styles.table}>
-                        <thead>
-                            <tr>
-                                {columns.map((col, index) => (
-                                    <th key={index}>{col}</th>
-                                ))}
-                                {showActions && <th>Acciones</th>}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {paginatedItems.map((item, rowIndex) => (
-                                <tr key={rowIndex}>
-                                    {columns.map((col, cellIndex) => (
-                                        <td key={cellIndex}>{item[col]}</td>
-                                    ))}
-                                    {showActions && (
-                                        <td className={styles["actions-column"]}>
-                                            <button onClick={() => console.log(`Editar ${item.nombre}`)}>Editar</button>
-                                            <button onClick={() => console.log(`Eliminar ${item.nombre}`)}>Eliminar</button>
-                                            {extraActions &&
-                                                extraActions.map((action, index) => (
-                                                    <button key={index} onClick={() => action.onClick(item)}>
-                                                        {action.label}
-                                                    </button>
-                                                ))}
-                                        </td>
-                                    )}
-                                </tr>
+        <>
+            <div className={styles.tableContainer} onScroll={handleScroll}>
+                <table className={styles.genericTable}>
+                    <thead>
+                        <tr>
+                            {columns.map((col, idx) => isColumnVisible(col.value) && (
+                                <th key={idx}>{col.label}</th>
                             ))}
-                        </tbody>
-                    </table>
-
-                    {children}
-
-                    {/* Pagination Component */}
-                    <Pagination
-                        currentPage={currentPage}
-                        totalPages={totalPages}
-                        onPageChange={setCurrentPage} // Pass the setter to handle page changes
-                    />
-                </>
-            )}
-        </div>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {loading ? (
+                            <tr>
+                                <td colSpan={columns.length} className={styles.loadingTd}>
+                                    <div className={styles.loadingContainer}>
+                                        Loading...
+                                    </div>
+                                </td>
+                            </tr>
+                        ) : data.length === 0 ? (
+                            <tr className={styles.noResults}>
+                                <td colSpan={columns.length}>
+                                    <Icon icon="exclamation" />
+                                    NoItems
+                                </td>
+                            </tr>
+                        ) : (
+                            data.slice(0, visibleRows).map((row, idx) => (
+                                <tr key={idx} className={`${styles.data} ${idx % 2 === 0 ? styles.evenRow : styles.oddRow}`}>
+                                    {columns.map((col) => isColumnVisible(col.value) && (
+                                        <td key={col.value}>
+                                            {row[col.value]}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </table>
+            </div>
+            <div className={styles.footer}>
+                <p>totalRows: {data.length}</p>
+            </div>
+        </>
     );
 };
 
-Table.propTypes = {
-    elements: PropTypes.arrayOf(PropTypes.object).isRequired,
-    showActions: PropTypes.bool,
-    extraActions: PropTypes.arrayOf(
-        PropTypes.shape({
-            label: PropTypes.string.isRequired,
-            onClick: PropTypes.func.isRequired,
-        })
-    ),
-    loading: PropTypes.bool,
-    itemsPerPage: PropTypes.number,
-    className: PropTypes.string,
-    children: PropTypes.node,
+Table.defaultProps = {
+    data: [],
+    visibleColumns: [],  // Si no se pasa, todas las columnas son visibles
+    loading: false,
 };
 
-Table.defaultProps = {
-    showActions: true,
-    extraActions: [],
-    loading: false,
-    itemsPerPage: 10,
-    className: "",
-    children: null,
+Table.propTypes = {
+    data: PropTypes.array,
+    visibleColumns: PropTypes.array,
+    loading: PropTypes.bool,
 };
 
 export default Table;
