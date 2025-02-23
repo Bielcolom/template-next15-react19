3e0a17fc3cdbc9139d1ff82bbc0c34934e28a2c4
Gabriel <gabrielcolomoll@gmail.com>
2025-02-23 12:58:44 +0100

    refactor/button

--- src/app/components/base/Button/index.jsx
+++ src/app/components/base/Button/index.jsx
@@ -1,39 +1,123 @@
 import PropTypes from "prop-types";
 import styles from "./button.module.scss";
+import Icon from "../Icon";
+import Link from "next/link";
+
+export const BUTTON_STYLE_TYPES = {
+    primary: "primary",
+    primaryDark: "primaryDark",
+    primaryDarker: "primaryDarker",
+    primaryLight: "primaryLight",
+    primaryLighter: "primaryLighter",
+    secondary: "secondary",
+    secondaryDark: "secondaryDark",
+    secondaryDarker: "secondaryDarker",
+    secondaryLight: "secondaryLight",
+    secondaryLighter: "secondaryLighter",
+    accent: "accent",
+    gray: "gray",
+    black: "black",
+    white: "white",
+    transparent: "transparent",
+};
 
 const Button = ({
-    text,
-    type = "button",
-    onClick,
-    disabled = false,
+    buttonRef,
+    children,
     className,
-    style = {},
+    disableStyleType = false,
+    disabled = false,
+    target = "",
     iconLeft,
-    ...props
+    iconRight,
+    inverted = false,
+    onClick = () => { },
+    styleOnPress = false,
+    text,
+    to,
+    type = "button",
+    styleType,
+    ...rest
 }) => {
-    return (
+    let computedClassName = styles.button;
+
+    if (styleOnPress) {
+
+        computedClassName += ` ${styles.btnPress}`;
+    }
+    if (!disableStyleType && styleType) {
+        computedClassName += ` ${styles[styleType]}`;
+    }
+    if (inverted) {
+        computedClassName += ` ${styles.inverted}`;
+    }
+    if (className) {
+        computedClassName += ` ${className}`;
+    }
+    if (disabled) {
+        computedClassName += ` ${styles.disabled}`;
+    }
+
+    const handleClick = (e) => {
+        if (disabled) {
+            e.preventDefault();
+        } else {
+            onClick(e);
+        }
+    };
+
+    const content = (
+        <>
+            {iconLeft && <Icon icon={`${iconLeft} left`} className={text ? styles.withMargin : ""} />}
+            {text && <span>{text}</span>}
+            {children}
+            {iconRight && <Icon icon={`${iconRight} right`} className={text ? styles.withMargin : ""} />}
+        </>
+    );
+
+    return to ? (
+        <Link href={to} passHref>
+            <a
+                {...rest}
+                ref={buttonRef}
+                className={computedClassName}
+                target={target}
+                rel="noopener noreferrer"
+                onClick={handleClick}
+            >
+                {content}
+            </a>
+        </Link>
+    ) : (
         <button
+            {...rest}
+            ref={buttonRef}
             type={type}
-            onClick={onClick}
+            className={computedClassName}
             disabled={disabled}
-            className={`${styles.button} ${className}`}
-            style={style}
-            {...props}
+            onClick={handleClick}
         >
-            {iconLeft && <span className={styles.iconLeft}>{iconLeft}</span>}
-            {text}
+            {content}
         </button>
     );
 };
 
 Button.propTypes = {
-    text: PropTypes.node.isRequired,
-    type: PropTypes.oneOf(["button", "submit", "reset"]),
-    onClick: PropTypes.func,
-    disabled: PropTypes.bool,
+    buttonRef: PropTypes.any,
+    children: PropTypes.node,
     className: PropTypes.string,
-    style: PropTypes.object,
-    iconLeft: PropTypes.node,
+    disableStyleType: PropTypes.bool,
+    disabled: PropTypes.bool,
+    target: PropTypes.string,
+    iconLeft: PropTypes.string,
+    iconRight: PropTypes.string,
+    inverted: PropTypes.bool,
+    onClick: PropTypes.func,
+    styleOnPress: PropTypes.bool,
+    styleType: PropTypes.oneOf(Object.values(BUTTON_STYLE_TYPES)),
+    text: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    to: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    type: PropTypes.string,
 };
 
 export default Button;
