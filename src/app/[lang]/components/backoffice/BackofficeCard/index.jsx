9e2415846ad79e50864469329a984baaf3452909
Bielcolom <gabrielcolomoll@gmail.com>
2026-04-18 12:51:48 +0200

    feat: update BackofficeCard layout to support header and icon, enhance styling for better presentation

--- src/app/[lang]/components/backoffice/BackofficeCard/index.jsx
+++ src/app/[lang]/components/backoffice/BackofficeCard/index.jsx
@@ -1,15 +1,16 @@
+// handoff/src/app/[lang]/components/backoffice/BackofficeCard/index.jsx
+// Actualizado para soportar icono + layout con header.
+
 import PropTypes from "prop-types";
 import styles from "./backofficeCard.module.scss";
 
-const BackofficeCard = ({
-    text,
-    number,
-    icon,
-}) => {
+const BackofficeCard = ({ text, number, icon }) => {
     return (
         <div className={styles.backofficeClient}>
-            <span className={styles.text}>{text}</span>
-            {icon && <span className={styles.icon}>{icon}</span>}
+            <div className={styles.header}>
+                <span className={styles.text}>{text}</span>
+                {icon && <span className={styles.icon}>{icon}</span>}
+            </div>
             <span className={styles.number}>{number}</span>
         </div>
     );
@@ -17,13 +18,10 @@ const BackofficeCard = ({
 
 BackofficeCard.propTypes = {
     text: PropTypes.string.isRequired,
-    number: PropTypes.number.isRequired,
+    number: PropTypes.number,
     icon: PropTypes.node,
 };
 
-BackofficeCard.defaultProps = {
-    number: 0,
-    icon: null,
-};
+BackofficeCard.defaultProps = { number: 0, icon: null };
 
 export default BackofficeCard;
